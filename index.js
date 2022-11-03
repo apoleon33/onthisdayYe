7c558cdc233cfd6407ae734b7547eeec7b55a9d4
apoleon33 <simondumesny@gmail.com>
2022-11-03 19:03:09 +0100

    better website

--- index.js
+++ index.js
@@ -4,7 +4,29 @@ var mois = date.getMonth() + 1;
 var annee = date.getFullYear();
 
 const text = document.getElementById("date");
+const main = document.getElementById("main");
+const quoteTitle = document.getElementById("quoteTitle");
+const actualQuote = document.getElementById("quote");
+const authorQuoteHolder = document.getElementById("authorQuoteHolder");
+
 text.textContent = "On this day Ye ";
+
+async function makeQuote() {
+  let url = "https://api.kanye.rest/";
+  let response = await fetch(url);
+
+  let quoteJson = await response.json();
+
+  actualQuote.textContent = `"`;
+  actualQuote.textContent += quoteJson.quote;
+  actualQuote.textContent += `"`;
+
+  let authorQuote = document.createElement("h2");
+  authorQuote.textContent = "- Kanye West.";
+  authorQuoteHolder.appendChild(authorQuote);
+
+  return quoteJson.quote;
+}
 
 try {
   var numberOfEvents = yeData[mois][jour].length;
@@ -18,4 +40,7 @@ try {
   }
 } catch (err) {
   text.textContent += "did absolutely nothing.";
+
+  quoteTitle.textContent = "But for sure he said:";
+  makeQuote();
 }
